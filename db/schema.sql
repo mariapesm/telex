cafefea3ab48f1239771425b53596f0edd404480
Will Leinweber <will@bitfission.com>
2014-08-13 17:14:18 -0700

    check in schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,7 @@
 --
 
 SET statement_timeout = 0;
+SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
@@ -34,6 +35,50 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 --
 
 COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: producers; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE producers (
+    uuid uuid DEFAULT uuid_generate_v4() NOT NULL,
+    name text NOT NULL,
+    encrypted_api_key text,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone
+);
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE schema_migrations (
+    filename text NOT NULL
+);
+
+
+--
+-- Name: producers_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY producers
+    ADD CONSTRAINT producers_pkey PRIMARY KEY (uuid);
+
+
+--
+-- Name: schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (filename);
 
 
 --
