cafe69fcb2451ce5a4adcaf9489700b4187deb67
Will Leinweber <will@bitfission.com>
2014-08-18 15:45:19 -0700

    send data in producer messages endpoint spec

--- spec/endpoints/producer/messages_spec.rb
+++ spec/endpoints/producer/messages_spec.rb
@@ -7,8 +7,16 @@ describe Endpoints::Producer::Messages do
     Endpoints::Producer::Messages  end
 
   describe "POST /messages" do
+    before do
+      @message_body = {
+        title: 'Congratulations',
+        body: 'You are a winner',
+        target: {type: 'user', id: 'whatever'}
+      }
+    end
+
     it "succeeds" do
-      post "/messages",MultiJson.encode({})
+      post "/messages", MultiJson.encode(@message_body)
       expect(last_response.status).to eq(201)
     end
   end
