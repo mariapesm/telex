cafe746402ec6a7b1e071fde14db31cf86d5501e
Will Leinweber <will@bitfission.com>
2014-08-20 14:54:03 -0700

    disable rollbar in tests

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,5 +1,7 @@
-Rollbar.configure do |config|
-  config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
-  config.use_sucker_punch
+unless Config.rack_env == 'test'
+  Rollbar.configure do |config|
+    config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
+    config.use_sucker_punch
+  end
 end
 
