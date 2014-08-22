cafe80c90824d8d1ddabc904516718bd25309f55
Will Leinweber <will@bitfission.com>
2014-08-22 16:24:56 -0700

    reformat ApiKey

--- lib/telex/api_key.rb
+++ lib/telex/api_key.rb
@@ -1,23 +1,21 @@
 require 'openssl'
 
-module Telex
-  module ApiKey
-    extend self
+module Telex::ApiKey
+  extend self
 
-    def encrypt(raw_key)
-      OpenSSL::HMAC.hexdigest("sha256", Config.api_key_hmac_secret, raw_key)
-    end
+  def encrypt(raw_key)
+    OpenSSL::HMAC.hexdigest("sha256", Config.api_key_hmac_secret, raw_key)
+  end
 
-    def compare(encrypted_key, given_key)
-      time_constant_compare(encrypted_key, encrypt(given_key))
-    end
+  def compare(encrypted_key, given_key)
+    time_constant_compare(encrypted_key, encrypt(given_key))
+  end
 
-    private
+  private
 
-    def time_constant_compare(a, b)
-      check = a.bytesize ^ b.bytesize
-      a.bytes.zip(b.bytes) { |x,y| check |= x^y }
-      check == 0
-    end
+  def time_constant_compare(a, b)
+    check = a.bytesize ^ b.bytesize
+    a.bytes.zip(b.bytes) { |x,y| check |= x^y }
+    check == 0
   end
 end
