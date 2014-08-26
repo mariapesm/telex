cafeece3c3280a78f8a3ed216c95bff45340541c
Will Leinweber <will@bitfission.com>
2014-08-26 16:53:50 -0700

    progress on the actual plexer

--- lib/mediators/messages/plexer.rb
+++ lib/mediators/messages/plexer.rb
@@ -1,11 +1,27 @@
 module Mediators::Messages
   class Plexer < Mediators::Base
-    def initialize(message:)
+    attr_reader :user_ids
+
+    def initialize(message:, user_finder: nil)
       @message = message
+      @user_finder = user_finder
+      @user_ids = []
     end
 
     def call
+      get_users
+    end
 
+    def get_users
+      @user_ids = user_finder.call(@message)
+    end
+
+    private
+
+    def user_finder
+      @user_finder ||= begin
+
+                       end
     end
   end
 end
--- spec/mediators/messages/plexer_spec.rb
+++ spec/mediators/messages/plexer_spec.rb
@@ -1,5 +1,17 @@
 require "spec_helper"
 
-describe Mediators::Messages::Plexer do
-  
+describe Mediators::Messages::Plexer, '#get_users' do
+  before do
+    @message = instance_double(Message)
+    @user_finder = double('user finder')
+    @plexer = described_class.new(message: @message, user_finder: @user_finder)
+  end
+
+  it 'uses the user finder to set users' do
+    expect(@plexer.user_ids).to eq([])
+    expect(@user_finder).to receive(:call).with(@message).and_return(['id1', 'id2'])
+    @plexer.get_users
+    expect(@plexer.user_ids).to eq(['id1', 'id2'])
+  end
+
 end
