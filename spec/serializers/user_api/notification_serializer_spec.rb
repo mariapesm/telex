cafeec5d89949c1109b10263c061ab5238979922
Will Leinweber <will@bitfission.com>
2014-10-30 13:49:15 -0700

    whitespace

--- spec/serializers/user_api/notification_serializer_spec.rb
+++ spec/serializers/user_api/notification_serializer_spec.rb
@@ -6,10 +6,10 @@ describe Serializers::UserAPI::NotificationSerializer do
     @notes = Mediators::Notifications::Lister.run(user: @note.user)
   end
 
-    it 'can use whatever the lister mediator generates' do
-      sz = described_class.new(:default)
-      json = MultiJson.encode(sz.serialize(@notes))
-      expect(json).to match(@note.message.body)
-      expect(json).to match('2012-02-02T00:00:00Z') # the Z is required for firefox!
-    end
+  it 'can use whatever the lister mediator generates' do
+    sz = described_class.new(:default)
+    json = MultiJson.encode(sz.serialize(@notes))
+    expect(json).to match(@note.message.body)
+    expect(json).to match('2012-02-02T00:00:00Z') # the Z is required for firefox!
+  end
 end
