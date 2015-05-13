02e02dcf152f8837186650826d075d831adf7ae7
Pedro Belo <pedro@heroku.com>
2015-05-13 15:15:14 -0700

    get rid of the unit
    I think this is confusing l2met when there is no value

--- lib/telex/sidekiq_instrumenter.rb
+++ lib/telex/sidekiq_instrumenter.rb
@@ -2,11 +2,11 @@ module Telex
   class SidekiqInstrumenter
     def call(worker, msg, queue, &block)
       begin
-        Telex::Sample.measure("jobs.locked", units: "jobs")
+        Telex::Sample.measure("jobs.locked")
         Telex::Sample.measure("jobs.duration", &block)
-        Telex::Sample.measure("jobs.success", units: "jobs")
+        Telex::Sample.measure("jobs.success")
       rescue Exception
-        Telex::Sample.measure("jobs.failure", units: "jobs")
+        Telex::Sample.measure("jobs.failure")
         raise
       end
     end
