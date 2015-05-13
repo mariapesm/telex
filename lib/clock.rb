fb7ef43bca1d1a8acce694039e640d8a6da669db
Pedro Belo <pedro@heroku.com>
2015-05-13 14:02:21 -0700

    simplify

--- lib/clock.rb
+++ lib/clock.rb
@@ -5,6 +5,6 @@ require 'sidekiq/api'
 module Clockwork
   every(10.seconds, 'monitor_queue') do
     stats = Sidekiq::Stats.new
-    Telex::Sample.count "jobs.queue.count", value: stats.enqueued
+    Telex::Sample.count "jobs.queue", value: stats.enqueued
   end
 end
