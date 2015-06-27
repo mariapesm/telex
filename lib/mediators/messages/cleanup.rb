cafeb8e3b398f07683f59b33982cf2d182518044
Will Leinweber <will@bitfission.com>
2015-06-26 17:35:46 -0700

    indent

--- lib/mediators/messages/cleanup.rb
+++ lib/mediators/messages/cleanup.rb
@@ -3,16 +3,16 @@ module Mediators::Messages
     def call
       db = Sequel::Model.db
       db.transaction do
-      db["DELETE FROM followups
-            USING messages
-            WHERE messages.id=followups.message_id
-              AND messages.created_at < now()-'3 months'::interval"].all
-      db["DELETE FROM notifications
-            USING messages
-            WHERE messages.id=notifications.message_id
-              AND messages.created_at < now()-'3 months'::interval"].all
-      db["DELETE FROM messages
-            WHERE messages.created_at < now()-'3 months'::interval"].all
+        db["DELETE FROM followups
+              USING messages
+              WHERE messages.id=followups.message_id
+                AND messages.created_at < now()-'3 months'::interval"].all
+        db["DELETE FROM notifications
+              USING messages
+              WHERE messages.id=notifications.message_id
+                AND messages.created_at < now()-'3 months'::interval"].all
+        db["DELETE FROM messages
+              WHERE messages.created_at < now()-'3 months'::interval"].all
       end
     end
   end
