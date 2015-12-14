843ac111aa1a3f026c3fd6b34bc8a0573ea2d866
Evan Light <elight@heroku.com>
2015-12-14 13:57:14 -0500

    Log the message payload before demultiplexing

--- lib/mediators/messages/creator.rb
+++ lib/mediators/messages/creator.rb
@@ -1,18 +1,20 @@
 module Mediators::Messages
   class Creator < Mediators::Base
     def initialize(producer:, title:, body:, action_label:, action_url:, target_type:, target_id:)
-      @message = Message.new(
-                   producer_id: producer.id,
-                   title: title,
-                   body: body,
-                   action_label: action_label,
-                   action_url: action_url,
-                   target_type: target_type,
-                   target_id: target_id)
+      @args = {
+        producer_id: producer.id,
+        title: title,
+        body: body,
+        action_label: action_label,
+        action_url: action_url,
+        target_type: target_type,
+        target_id: target_id
+      }
     end
 
     def call
-      @message.save
+      Message.new(@args).save
+      Pliny.log(@args.merge(messages_creator: true, telex: true))
       Jobs::MessagePlex.perform_async(@message.id)
       Telex::Sample.count "messages"
       @message
