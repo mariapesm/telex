17dd396848cde926dfb7519182f10928e4bb9703
Doug McInnes <doug@dougmcinnes.com>
2016-05-19 09:27:53 -0700

    handle uniqueness validations, message send failures

--- spec/mediators/notifications/creator_spec.rb
+++ spec/mediators/notifications/creator_spec.rb
@@ -9,5 +9,22 @@ describe Mediators::Notifications::Creator do
     result = nil
     expect{ result = @creator.call }.to change(Notification, :count).by(1)
     expect(result).to be_instance_of(Notification)
+    expect(Mail::TestMailer.deliveries.count).to be(1)
+  end
+
+  it 'does not send duplicate messages' do
+    expect do
+      @creator.call
+      @creator.call
+    end.to change(Notification, :count).by(1)
+
+    expect(Mail::TestMailer.deliveries.count).to be(1)
+  end
+
+  it 'removes the Notification object on message send failures' do
+    allow(Telex::Emailer).to receive(:new) { raise Net::ReadTimeout }
+    expect{ @creator.call }.to raise_error Net::ReadTimeout
+    expect(Mail::TestMailer.deliveries.count).to be(0)
+    expect(Notification.count).to be(0)
   end
 end
