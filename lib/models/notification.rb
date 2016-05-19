997b6c0d24a511311eda3789eabbac82a8f80b24
Doug McInnes <doug@dougmcinnes.com>
2016-05-19 09:26:36 -0700

    add validation on user_id-message_id uniqueness

--- lib/models/notification.rb
+++ lib/models/notification.rb
@@ -3,5 +3,10 @@ class Notification < Sequel::Model
   many_to_one :message
 
   plugin :timestamps
+  plugin :validation_helpers
 
+  def validate
+    super
+    validates_unique %i[user message]
+  end
 end
