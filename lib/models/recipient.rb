768b15ef5f3c89e80e095e9f260b1d70bb15d0fe
Cyril David <cyx@cyx.is>
2016-07-02 12:48:13 -0700

    Simplify email validation, reuse URI.regexp

--- lib/models/recipient.rb
+++ lib/models/recipient.rb
@@ -1,15 +1,7 @@
 class Recipient < Sequel::Model
   VERIFICATION_TOKEN_TTL = 7200
 
-  EMAIL = /\A([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*
-           [\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@
-           ((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+
-           [a-z]{2,12})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)\z/ix
-
-  URL = /\A(http|https):\/\/([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,12}|(2
-         5[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}
-         |localhost)(:[0-9]{1,5})?(\/.*)?\z/ix
-
+  EMAIL = /@/
   TOKEN = %r(%{token})
   ID = %r(%{id})
 
@@ -32,7 +24,7 @@ class Recipient < Sequel::Model
     super
     validates_presence %i(app_id callback_url email)
     validates_format EMAIL, :email
-    validates_format URL, :callback_url
+    validates_format URI.regexp, :callback_url
     validates_format TOKEN, :callback_url
     validates_format ID, :callback_url
   end
