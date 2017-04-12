21d702fc4a61f8eefeec4f4b05f94d728b8f0aff
Evan Light <elight@heroku.com>
2017-04-12 12:35:14 -0400

    Remove superfluous line

--- config/initializers/log.rb
+++ config/initializers/log.rb
@@ -1,5 +1,3 @@
 Pliny.default_context = { app: "telex" }
 
 Pliny.log_scrubber = BlacklistHash.build(fields_to_scrub: Rollbar::Blanket.fields)
-
-BlacklistHash.default_fields = Rollbar::Blanket.fields
