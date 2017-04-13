f82e187061175575912fdf3793d4c85a33b97da1
Evan Light <elight@heroku.com>
2017-04-13 12:46:31 -0400

    scrub URLs of sensitive fields

--- config/initializers/log.rb
+++ config/initializers/log.rb
@@ -1,3 +1,3 @@
 Pliny.default_context = { app: "telex" }
 
-Pliny.log_scrubber = BlacklistHash.build(fields_to_scrub: Rollbar::Blanket.fields)
+Pliny.log_scrubber = BlacklistHash.build(fields_to_scrub: Rollbar::Blanket.fields, scrub_urls: true)
